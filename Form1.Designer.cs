7e090567bfb5ec39ccf183e1703583bcfb0931ed
Fábián Gábor <info@fabiangabor.com>
2019-10-24 20:19:32 +0200

    Solve realtime on input change

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -31,13 +31,15 @@
             this.components = new System.ComponentModel.Container();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBoxSolution = new System.Windows.Forms.TextBox();
+            this.textBoxSolution1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxSolution2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // contextMenuStrip1
@@ -48,51 +50,41 @@
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.textBox1.Location = new System.Drawing.Point(156, 164);
+            this.textBox1.Location = new System.Drawing.Point(142, 126);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(42, 32);
             this.textBox1.TabIndex = 1;
             this.textBox1.Text = "1";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // button1
-            // 
-            this.button1.AutoSize = true;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.button1.Location = new System.Drawing.Point(243, 295);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(79, 38);
-            this.button1.TabIndex = 99;
-            this.button1.Text = "Solve";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.textBox2.Location = new System.Drawing.Point(265, 164);
+            this.textBox2.Location = new System.Drawing.Point(251, 126);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(42, 32);
             this.textBox2.TabIndex = 2;
             this.textBox2.Text = "2";
             this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.textBox3.Location = new System.Drawing.Point(367, 164);
+            this.textBox3.Location = new System.Drawing.Point(353, 126);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(42, 32);
             this.textBox3.TabIndex = 3;
             this.textBox3.Text = "-4";
             this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(204, 167);
+            this.label1.Location = new System.Drawing.Point(190, 129);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 26);
             this.label1.TabIndex = 5;
@@ -103,7 +95,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.label2.Location = new System.Drawing.Point(313, 167);
+            this.label2.Location = new System.Drawing.Point(299, 129);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(48, 26);
             this.label2.TabIndex = 100;
@@ -114,23 +106,54 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.label3.Location = new System.Drawing.Point(415, 167);
+            this.label3.Location = new System.Drawing.Point(401, 129);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(43, 26);
             this.label3.TabIndex = 101;
             this.label3.Text = "= 0";
             this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
             // 
-            // textBoxSolution
+            // textBoxSolution1
             // 
-            this.textBoxSolution.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.textBoxSolution.Location = new System.Drawing.Point(156, 240);
-            this.textBoxSolution.Name = "textBoxSolution";
-            this.textBoxSolution.ReadOnly = true;
-            this.textBoxSolution.Size = new System.Drawing.Size(253, 32);
-            this.textBoxSolution.TabIndex = 102;
-            this.textBoxSolution.Text = "x1 = 1   x2 = 1";
-            this.textBoxSolution.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxSolution1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.textBoxSolution1.Location = new System.Drawing.Point(195, 202);
+            this.textBoxSolution1.Name = "textBoxSolution1";
+            this.textBoxSolution1.ReadOnly = true;
+            this.textBoxSolution1.Size = new System.Drawing.Size(249, 32);
+            this.textBoxSolution1.TabIndex = 102;
+            this.textBoxSolution1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(140, 205);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 26);
+            this.label4.TabIndex = 104;
+            this.label4.Text = "x₁ =";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(140, 243);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(49, 26);
+            this.label5.TabIndex = 106;
+            this.label5.Text = "x₂ =";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBoxSolution2
+            // 
+            this.textBoxSolution2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.textBoxSolution2.Location = new System.Drawing.Point(195, 240);
+            this.textBoxSolution2.Name = "textBoxSolution2";
+            this.textBoxSolution2.ReadOnly = true;
+            this.textBoxSolution2.Size = new System.Drawing.Size(249, 32);
+            this.textBoxSolution2.TabIndex = 105;
+            this.textBoxSolution2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // Form1
             // 
@@ -138,13 +161,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(584, 361);
-            this.Controls.Add(this.textBoxSolution);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxSolution2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxSolution1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -156,13 +181,15 @@
         #endregion
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBoxSolution;
+        private System.Windows.Forms.TextBox textBoxSolution1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxSolution2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,23 +15,58 @@ namespace Quadratic_equation
         public Form1()
         {
             InitializeComponent();
+            Solve();
+        }
+
+        private void Solve()
+        {
+            var isNumeric_a = int.TryParse(textBox1.Text, out int a);
+            var isNumeric_b = int.TryParse(textBox2.Text, out int b);
+            var isNumeric_c = int.TryParse(textBox3.Text, out int c);
+
+            if (isNumeric_a & isNumeric_b & isNumeric_c) {                
+                double x1 = (-b + Math.Sqrt(Math.Pow(b, 2) - (4 * a * c))) / (2 * a);
+                double x2 = (-b - Math.Sqrt(Math.Pow(b, 2) - (4 * a * c))) / (2 * a);
+
+                textBoxSolution1.Text = Convert.ToString(x1);
+                textBoxSolution2.Text = Convert.ToString(x2);
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-
+            Solve();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            /*
             Int32 a = Convert.ToInt32(textBox1.Text);
             Int32 b = Convert.ToInt32(textBox2.Text);
             Int32 c = Convert.ToInt32(textBox3.Text);
 
             double x1 = (-b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
             double x2 = (-b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
+            
 
             textBoxSolution.Text = Convert.ToString(x1 + ", " + x2);
+            */
+            Solve();
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            Solve();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Solve();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            Solve();
         }
     }
 }
